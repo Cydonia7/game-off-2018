78ed8bab9a1fb479ce8e9537266d5501e055533b
Thomas Royer <cydonia572@gmail.com>
2018-11-25 14:52:00 +0100

    Turrets cannot collide

--- CannonSpawner.gd
+++ CannonSpawner.gd
@@ -6,24 +6,31 @@ export(int) var cost
 
 var was_pressed = false
 var can_place_tower = true
+var is_colliding = false
 var money = 0
 
 func _enter_tree():
 	EventListener.add_listener(self, ['wave_started', 'wave_finished'])
 
+func on_turret_entered():
+	is_colliding = true
+
+func on_turret_exited():
+	is_colliding = false
+
 func _process(delta):
-	$Sprite.texture = cannonTexture
+	get_node('Area/Sprite').texture = cannonTexture
 	
 	if not (cannonScene and cannonTexture):
 		return
 	
 	set_global_position(get_global_mouse_position())
 	
-	var in_level = global_position.y + $Sprite.texture.get_height() / 2 < 600
+	var in_level = global_position.y + get_node('Area/Sprite').texture.get_height() / 2 < 600
 	var game_manager = get_game_manager()
 	var has_enough_money = get_game_manager().can_buy(cost)
 	
-	$Sprite.set_visible(in_level && can_place_tower && has_enough_money)
+	get_node('Area/Sprite').set_visible(in_level && !is_colliding && can_place_tower && has_enough_money)
 	
 	if not can_place_tower:
 		return
@@ -31,7 +38,7 @@ func _process(delta):
 	if was_pressed && !Input.is_mouse_button_pressed(BUTTON_LEFT):
 		was_pressed = false
 		
-		if in_level && has_enough_money:
+		if in_level && !is_colliding && has_enough_money:
 			var cannon = cannonScene.instance()
 			cannon.set_global_position(get_global_mouse_position())
 			get_node('../Level/Cannons').add_child(cannon)
